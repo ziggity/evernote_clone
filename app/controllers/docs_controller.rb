bbf445e35494c6dee83c972f5973bcce9fcf21b8
zach beecher <zachbeecher@gmail.com>
2017-10-19 22:18:52 -0700

    add docs and actions in controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,25 +1,42 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @docs = Doc.all.order("created_at DESC")
+  end
 
-def index
-end
+  def show
 
-def show
-end
+  end
 
-def new
-end
+  def new
+    @doc = Doc.new
+  end
 
-def create
-end
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
+  end
 
-def edit
-end
+  def edit
+  end
 
-def update
-end
+  def update
+  end
 
-def destroy
-end
+  def destroy
+  end
 
+  private
+    def find_doc
+        @doc = Doc.find(params[:id])
+    end
+
+    def doc_params
+      params.require(:doc).permit(:title, :content)
+    end
 end
